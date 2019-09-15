2e4533a0dbd7367450e0627141823ee46d300183
Siddharth Sen <40197136+siddharth-sen@users.noreply.github.com>
2019-09-15 10:36:26 +0530

    Create basic_data_structures.py

---
+++ basic_data_structures.py
@@ -0,0 +1,42 @@
+# python3
+
+from collections import namedtuple
+
+Bracket = namedtuple("Bracket", ["char", "position"])
+
+
+def find_mismatch(text):
+
+    opening_brackets_stack = []
+
+    for i, next in enumerate(text):
+        if next in "([{":
+            # Process opening bracket
+            opening_brackets_stack.append(Bracket(next, i+1))
+            pass
+
+        if next in ")]}":
+            # Process closing bracket
+            if len(opening_brackets_stack) == 0:
+                return print(i+1)
+            else:
+                top = opening_brackets_stack.pop()
+                if (top.char == '[' and next != ']') or (top.char == '{' and next != '}') or (top.char == '(' and next != ')'):
+                    return print(i+1)
+
+    if len(opening_brackets_stack) == 0:
+        return print('Success')
+    else:
+        return print(opening_brackets_stack[0][1])
+
+
+def main():
+    text = input()
+    find_mismatch(text)
+    # Printing answer, write your code here
+
+
+if __name__ == "__main__":
+    main()
+    
+    
